96b91d5510c3ecc3cf5fbab169073d7cbed219ea
Guilherme Teixeira <24235344+ggteixeira@users.noreply.github.com>
2025-08-08 11:33:32 -0300

    Records migration

--- Disqueteira/Migrations/20250808143248_Records.Designer.cs
+++ Disqueteira/Migrations/20250808143248_Records.Designer.cs
@@ -3,6 +3,7 @@ using Disqueteira.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Disqueteira.Migrations
 {
     [DbContext(typeof(DisqueteiraContext))]
-    partial class DisqueteiraContextModelSnapshot : ModelSnapshot
+    [Migration("20250808143248_Records")]
+    partial class Records
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
