d355376d154819fc162992c2c384861332b76e50
Guilherme Teixeira <24235344+ggteixeira@users.noreply.github.com>
2025-08-09 13:59:37 -0300

    Adds the migration after fixing the foreign key syntax

---
+++ Disqueteira/Migrations/20250808222918_Tries to create a foreign key correctly.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Disqueteira.Migrations
+{
+    /// <inheritdoc />
+    public partial class Triestocreateaforeignkeycorrectly : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "ArtistId",
+                table: "Records",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Records_ArtistId",
+                table: "Records",
+                column: "ArtistId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Records_Artists_ArtistId",
+                table: "Records",
+                column: "ArtistId",
+                principalTable: "Artists",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Records_Artists_ArtistId",
+                table: "Records");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Records_ArtistId",
+                table: "Records");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "ArtistId",
+                table: "Records",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+        }
+    }
+}
