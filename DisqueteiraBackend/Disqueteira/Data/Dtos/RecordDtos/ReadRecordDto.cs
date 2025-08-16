0785125af09bab6e4c80df8d2bc21f8a4d664c7b
Guilherme Teixeira <24235344+ggteixeira@users.noreply.github.com>
2025-08-15 21:36:59 -0300

    Removes unneeded item from DTO

--- DisqueteiraBackend/Disqueteira/Data/Dtos/RecordDtos/ReadRecordDto.cs
+++ DisqueteiraBackend/Disqueteira/Data/Dtos/RecordDtos/ReadRecordDto.cs
@@ -2,7 +2,7 @@ namespace Disqueteira.Data.Dtos.RecordDtos;
 
 public class ReadRecordDto
 {
-    public string Id { get; set; }
+    // public string Id { get; set; }
     public string Name { get; set; }
     public int Year { get; set; }
     public int ArtistId { get; set; }
