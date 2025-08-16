7133807efe05bffed6d532e00272473fcb57a216
Guilherme Teixeira <24235344+ggteixeira@users.noreply.github.com>
2025-08-15 21:37:15 -0300

    Moves provider

--- DisqueteiraFrontend/src/App.tsx
+++ DisqueteiraFrontend/src/App.tsx
@@ -1,16 +1,12 @@
 import { BrowserRouter } from 'react-router-dom';
 import { CssBaseline } from '@mui/material';
 import MiniDrawer from './components/MuiResponsibleDrawer';
-import { QueryClientProvider, useQueryClient } from '@tanstack/react-query';
 
 const App = () => {
-  const client = useQueryClient();
-
   return (
     <>
       <BrowserRouter>
         <CssBaseline />
-        <QueryClientProvider client={client}></QueryClientProvider>
         <MiniDrawer />
       </BrowserRouter>
     </>
--- DisqueteiraFrontend/src/main.tsx
+++ DisqueteiraFrontend/src/main.tsx
@@ -5,12 +5,17 @@ import store from './app/store';
 import React from 'react';
 import App from './App';
 import './main.css';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <Provider store={store}>
       <AppThemeProvider>
-        <App />
+        <QueryClientProvider client={queryClient}>
+          <App />
+        </QueryClientProvider>
       </AppThemeProvider>
     </Provider>
   </React.StrictMode>,
